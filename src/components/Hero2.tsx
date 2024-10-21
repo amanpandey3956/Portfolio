5756a25a063d5ef71c5af0adc146fe1b55ee080e
amanpandey3956 <amanpandey39563@gmail.com>
2024-10-22 01:51:51 +0530

    hero section update

--- src/components/Hero2.tsx
+++ src/components/Hero2.tsx
@@ -3,7 +3,7 @@ import { SparklesCore } from "./ui/sparkles";
 
 export function Hero2() {
   return (
-    <div className="min-h-screen relative w-full bg-gradient-to-b from-gray-1000 to-gray-1100 flex items-center justify-center overflow-hidden border-neutral-600">  
+    <div className="min-h-screen relative w-full bg-gradient-to-b from-gray-1000 to-gray-1100 flex flex-col items-center justify-center overflow-hidden border-neutral-600">
       <div className="w-full absolute inset-0 h-full">
         <SparklesCore
           id="tsparticlesfullpage2"
@@ -12,32 +12,40 @@ export function Hero2() {
           maxSize={1.4}
           particleDensity={100}
           className="w-full h-full"
-          particleColor="#FFFFFF" 
+          particleColor="#FFFFFF"
         />
       </div>
 
       <div className="relative z-10 w-full h-full flex flex-col lg:flex-row items-center lg:items-start justify-between p-4">
-  
-        <div className="w-full lg:w-2/3 h-full flex flex-col items-center lg:items-start justify-start mb-8 lg:mb-0">
-          <h1 className="mt-16 lg:mt-0 text-5xl font-bold bg-clip-text text-transparent text-white text-center lg:text-left lg:ml-48">
-            Let Me <span className="text-purple-300">Introduce</span> Myself
+        <div className="w-full lg:w-2/3 flex flex-col items-center lg:items-start justify-start mb-8 lg:mb-0 h-full">
+          <h1 className="mt-32 lg:mt-24 text-5xl font-bold bg-clip-text text-transparent text-white text-center lg:text-left lg:ml-48">
+            Let Me <span className="text-purple-400">Introduce</span> Myself
           </h1>
           <p className="mt-16 lg:mt-14 text-lg lg:text-justify font-semibold bg-clip-text text-transparent text-white text-justify lg:ml-24">
             I&apos;m a Frontend Focused Web Developer Driven by a passion for digital transformation, I am a versatile frontend developer specializing in creating seamless and engaging user experiences.
             <br /><br />
             As a dedicated frontend developer, I am committed to driving digital transformation through innovative web solutions. My approach combines technical proficiency with a strategic mindset, enabling me to translate complex challenges into effective, user-centric designs. My experience spans building dynamic applications with React.js, Next.js, TypeScript, and integrating libraries like Framer Motion for enhanced interactivity. I embrace emerging technologies and innovative frameworks to push the boundaries of what is possible in web development.
-            <br/><br/>
-            I&apos;m open to Job opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience then don &apos;t hesitate to contact me.
+            <br /><br />
+            I&apos;m open to Job opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience then don&apos;t hesitate to contact me.
           </p>
         </div>
 
-        <div className="w-full mt-14 lg:w-1/3 h-full flex items-center justify-center">
+        <div className="w-full lg:mt-40 lg:w-1/3 flex items-center justify-center h-full">
           <img
             src="/projects/myimg.jpg"
             alt="Your Image"
-            className="rounded-full w-[250px] h-[250px] md:w-[350px] md:h-[350px] object-cover shadow-lg"
+            className="rounded-full w-[250px] h-[250px] md:w-[350px] md:h-[350px] lg:w-[330px] lg:h-[330px] object-cover shadow-lg"
           />
         </div>
+      </div>
+
+      {/* Button Section */}
+      <div className="relative z-10 flex justify-center mt-12 lg:mt-12 mb-16 lg:mb-16">
+        <a href="https://linktr.ee/Aman.Pandey" target="_blank" rel="noopener noreferrer">
+          <button className="px-6 py-3 bg-purple-700 text-white font-semibold rounded-lg hover:bg-purple-500 transition-colors duration-300">
+            Connect with Me
+          </button>
+        </a>
       </div>
     </div>
   );
