bfdb0c7c73a536f87f0b5a28527b8b78d90b41e2
amanpandey3956 <amanpandey39563@gmail.com>
2025-04-14 02:37:22 +0530

    blog responsiveness added

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -43,7 +43,7 @@ export default async function BlogPage({ params }: Props) {
   const contentHtml = processedContent.toString()
 
   return (
-    <article className="text-gray-100 relative bg-gray-900 py-12 pt-56 overflow-auto">
+    <article className="text-gray-100 relative bg-gray-900 py-12 lg:px-0 md:px-12 sm:px-10 px-7 pt-28 md:pt-56 sm:pt-48 lg:pt-56 overflow-auto">
       <div className="overflow-hidden mx-auto max-w-5xl prose prose-invert prose-lg">
         <h1 className="mb-8 text-3xl md:text-4xl lg:text-3xl text-emerald-400 font-bold">{data.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
